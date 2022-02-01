df02b976c88ee49ca527c3f77811d2abf9cdd6c3
Aravind <aravindsreenandanam11@gmail.com>
2022-02-01 23:16:22 +0530

    ippo ellam shari aai

---
+++ app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Authentication;
+use Illuminate\Support\Facades\Hash;
+class AuthenticationController extends Controller
+{
+    //sign up detail store
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name'=>'required',
+            'email'=>'required|email|unique:authentications',
+            'password'=>'required',
+            'role'=>'required'
+            
+        ]);
+        $userdet=new Authentication();
+        $userdet->username=$request->name;
+        $userdet->email=$request->email;
+        $userdet->password=Hash::make($request->password);
+        $userdet->role=$request->role;
+        $userdet->save();
+ 
+        return redirect('/Login');
+    }
+    //sign in details store
+    public function check(Request $request)
+    {
+        $userinfo=Authentication::where('email','=',$request->email)->first();
+        if(!$userinfo){
+            return back()->with('failmsg','The mail id noes not exist please sign up');
+        }
+        else{
+            if(Hash::check($request->password,$userinfo->password))
+            {
+                $request->session()->put('user_id',$userinfo->user_id );
+                return redirect("/$userinfo->role");
+            }
+            else{
+                return back()->with('failmsg','Password is incorrect');
+            }
+        }
+        
+    }
+}
